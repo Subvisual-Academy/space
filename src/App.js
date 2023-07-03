d3a115f411060b4fcc8d2ea499508f0af47f7911
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-03 18:21:25 +0100

    Sign-up template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+ import './App.css';
+ import Background from './assets/Background.svg'
+ import Logo from './assets/spacecenter1.svg'
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div class="bg">
+
+      <img class="img-container" src={Background} alt="SpaceBg" />
+      <img class="logo" src={Logo} alt="SpaceBg" />
+
+      <div class="input-container">
+        <div class="input-box">
+          <form>
+            <input id="email" type="text" placeholder="E-mail" />
+            <input id="email" type="text" placeholder="Confirm e-mail" />
+            <input id="email" type="password" placeholder="Password" />
+            <input id="email" type="password" placeholder="Confirm Password" />
+            <button type='button' class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300">
+              <div class="submit">
+                Submit
+              </div>
+            </button>
+          </form>
+          <div id="account">Already have an account? <a className="underline text-blue-500" href="/login">Login</a></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
