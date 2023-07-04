4d0e73f630085ad3e5f8a523152ef3e29a81576c
Zamith <luis@zamith.pt>
2023-07-04 14:11:21 +0100

    Updates some of the styles

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import Logo from "./assets/spacecenter1.svg";
 
 function App() {
   return (
-    <div class="bg">
-      <img class="img-container" src={Background} alt="SpaceBg" />
-      <img class="logo" src={Logo} alt="SpaceBg" />
+    <div className="flex items-start flex-auto bg-cod-gray">
+      <div className="hidden md:block relative">
+        <img
+          className="max-h-screen basis-2/5 object-cover"
+          src={Background}
+          alt="SpaceBg"
+        />
+        <img
+          className="absolute h-96 w-72 top-[calc(50%-192px)] left-[calc(50%-144px)]"
+          src={Logo}
+          alt="SpaceBg"
+        />
+      </div>
 
-      <div class="input-container">
-        <div class="input-box">
-          <form>
-            <input id="email" type="text" placeholder="E-mail" />
-            <input id="email" type="text" placeholder="Confirm e-mail" />
-            <input id="email" type="password" placeholder="Password" />
-            <input id="email" type="password" placeholder="Confirm Password" />
-            <button
-              type="button"
-              class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300"
-            >
-              <div class="submit">Submit</div>
-            </button>
-          </form>
-          <div id="account">
-            Already have an account?{" "}
-            <a className="underline text-blue-500" href="/login">
-              Login
-            </a>
-          </div>
+      <div className="max-h-screen p-8 pt-28 flex flex-col grow items-center">
+        <form className="flex flex-col">
+          <input id="email" type="text" placeholder="E-mail" />
+          <input id="email" type="text" placeholder="Confirm e-mail" />
+          <input id="email" type="password" placeholder="Password" />
+          <input id="email" type="password" placeholder="Confirm Password" />
+          <button
+            type="button"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300"
+          >
+            <div className="submit">Submit</div>
+          </button>
+        </form>
+        <div id="account">
+          Already have an account?{" "}
+          <a className="underline text-blue-500" href="/login">
+            Login
+          </a>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "cod-gray": "#161616",
+      },
+    },
   },
   plugins: [],
-}
-
+};
