2d2cf8bfc49faf70c0f2845917479bdec51bdfd7
Tiago Passos Rodrigues <63853699+TiagoPR@users.noreply.github.com>
2023-07-11 11:48:38 +0100

    Sign-up template (#10)
    Why:
    
    * The user needs to create an account to acess the website.
    
    This change addresses the need by:
    
    * Exporting the SVG files from Figma and using the `img` tag, together with the black background. Adding the forms for the user inputs for creating an account and a button to submit. 
    
    Co-authored-by: Zamith <luis@zamith.pt>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
+import Background from "./assets/Background.svg";
+import Logo from "./assets/spacecenter1.svg";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex items-start flex-auto bg-cod-gray">
+      <div className="hidden md:block relative">
+        <img
+          className="max-h-screen basis-2/5 object-cover"
+          src={Background}
+          alt="Universe Background"
+        />
+        <img
+          className="absolute h-96 w-72 top-[calc(50%-theme(space.96)/2)] left-[calc(50%-theme(space.72)/2)]"
+          src={Logo}
+          alt="Space Center Logo"
+        />
+      </div>
+
+      <div className="max-h-screen p-8 mt-28 flex flex-col grow items-center">
+        <button
+          type="button"
+          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300 w-1/2 max-w-screen-sm"
+        >
+          <div className="border-dove-gray border-2 rounded-3xl pt-4 text-silver text-xl h-16 mt-12">
+            Continue with Google
+          </div>
+        </button>
+        <form className="flex flex-col w-9/12 max-w-screen-sm gap-12 mt-12">
+          <input
+            type="text"
+            placeholder="E-mail"
+            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
+          />
+          <input
+            type="text"
+            placeholder="Confirm e-mail"
+            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
+          />
+          <button
+            type="button"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300"
+          >
+            <div className="bg-med-purple rounded-3xl pt-3 text-alto text-3xl h-16">
+              Submit
+            </div>
+          </button>
+          <div className="text-silver text-2xl text-center">
+            Already have an account?{" "}
+            <a className="underline text-blue-500" href="/login">
+              Login
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "cod-gray": "#161616",
+        "med-purple": "#8556e0",
+        "alto": "#dcdcdc",
+        "silver": "#bcb9b9",
+        "gray": "#8C8C8C",
+        "mine-shaft": "#303030",
+        "dove-gray": "#636363"
+      },
+    },
   },
   plugins: [],
-}
-
+};
