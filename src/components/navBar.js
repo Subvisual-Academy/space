7048bf21ffbf315cf88b9af6b6a427297fc12591
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-14 14:15:03 +0100

    code check

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,21 +1,24 @@
 import Logo from "../assets/spacecenterlogo.svg";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className="bg-mine-shaft items-center p-4 flex gap-8 max-h-28">
-      <a href="/home">
+      <Link to="/home">
         <img
           className=" p-8 items-center gap-8"
           src={Logo}
           alt="Space Center Logo"
         />
-      </a>
-      <a href="/home">
-        <div className="flex-col text-white text-3xl">
-          <h1> Space</h1>
-          <h1> Center</h1>
-        </div>
-      </a>
+      </Link>
+      <Link to="/home">
+        <h1 className="text-white text-3xl">
+          {" "}
+          Space
+          <br />
+          Center
+        </h1>
+      </Link>
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,23 @@
 import "./font.css";
 import NavBar from "../components/navBar";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Home() {
-  let navigate = useNavigate();
-
   return (
     <div className="bg-cod-gray font absolute h-full w-full">
       <NavBar />
       <h1 className="text-white m-32 text-5xl">Space Center Activities</h1>
       <div className="m-32 flex gap-32">
-        <button
-          onClick={() => navigate("/question")}
-          className="rounded-3xl bg-cornflower-blue text-white h-64 w-64"
-        >
-          QUESTION OF THE WEEK
-        </button>
-        <button
-          onClick={() => navigate("/friend")}
-          className="rounded-3xl bg-cornflower-blue text-white p-4 h-64 w-64"
-        >
-          FRIEND OF THE WEEK
-        </button>
+        <Link to="/question">
+          <button className="rounded-3xl bg-cornflower-blue text-white h-64 w-64">
+            QUESTION OF THE WEEK
+          </button>
+        </Link>
+        <Link to="/friend">
+          <button className="rounded-3xl bg-cornflower-blue text-white p-4 h-64 w-64">
+            FRIEND OF THE WEEK
+          </button>
+        </Link>
       </div>
     </div>
   );
