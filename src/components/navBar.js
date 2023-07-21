96d290fff29ce970f09a90f43f9e3486412b64fb
MFJess <jessica2001mf@gmail.com>
2023-07-21 18:39:46 +0100

    Add avatar profile. Fix spacing.

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,11 +5,7 @@ function NavBar() {
   return (
     <div className="bg-navbar-gray items-center p-4 flex gap-8 max-h-16">
       <Link to="/home">
-        <img
-          className="ml-4 mr-60"
-          src={Logo}
-          alt="Space Center Logo"
-        />
+        <img className="ml-4 mr-60" src={Logo} alt="Space Center Logo" />
       </Link>
 
       <Link to="/home">
@@ -17,11 +13,15 @@ function NavBar() {
       </Link>
 
       <Link to="/question">
-        <button className="text-navbar-components-gray mr-8">Weekly Question</button>
+        <button className="text-navbar-components-gray mr-8">
+          Weekly Question
+        </button>
       </Link>
 
       <Link to="/friend">
-        <button className="text-navbar-components-gray mr-8">Weekly Friend</button>
+        <button className="text-navbar-components-gray mr-8">
+          Weekly Friend
+        </button>
       </Link>
     </div>
   );
--- src/pages/friend.js
+++ src/pages/friend.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Background from "../assets/beautiful-shining-stars-night-sky 1.png";
 import AstronautPointing from "../assets/astronaut pointing 1.png";
-import AstronautChilling from "../assets/astronaut-chilling.png"
+import AstronautChilling from "../assets/astronaut-chilling.png";
+import Avatar from "../assets/Avatar profile.png"
 import { useEffect } from "react";
 import NavBar from "../components/navBar";
 import { GET } from "../utils/fetch";
@@ -17,7 +18,7 @@ const activity = [
   "Checkers",
 ];
 
-const current_user_id = 4; //localStorage.getItem("current");
+const current_user_id = 2; //localStorage.getItem("current");
 
 const getFriend = async () => {
   var response = await GET("/friends/" + current_user_id);
@@ -53,76 +54,102 @@ function Friend() {
           <div className="relative z-10">
             <NavBar />
             <div className="flex">
-
               <div className="w-2/3 h-full">
-
                 <div className="text-white ml-72 mt-20 text-5xl font-bold">
                   Vacation week!
                 </div>
 
                 <div className="w-[50%] ml-72 mt-16 text-base text-white">
                   Yey, look at you! So lucky, this week you’ve got a little
-                  vacation from this task. Now, when you have more time, make sure
-                  you answer the question of this week and relax. See you next
-                  week, we’ve got someone very cool in store for you.{" "}
+                  vacation from this task. Now, when you have more time, make
+                  sure you answer the question of this week and relax. See you
+                  next week, we’ve got someone very cool in store for you.{" "}
                 </div>
-
+                
                 <img
                   className="ml-96 mt-12 h-3/5 relative z-10"
                   src={AstronautChilling}
                   alt="AstronautChilling"
                 />
-
               </div>
 
               <div className="w-1/3 h-[1080px] bg-bkg-purple flex-shrink-0">
-
                 <div className="text-white mt-[30px] text-center text-[32px] font-bold">
                   Suggested Activities
                 </div>
 
                 <div className="grid gap-8 justify-center mt-12">
                   {activity.map((activity) => (
-                    <button class="w-[250px] h-[65px] bg-button-gray text-white font-bold rounded-[27px] shadow-md">
+                    <button class="w-72 h-16 bg-button-gray text-white font-bold rounded-[27px] shadow-md">
                       {activity}
                     </button>
                   ))}
                 </div>
-
               </div>
-
-            </div> 
+            </div>
           </div>
         </div>
-
-          ) : (
-
+      ) : (
         <div>
-          <div className="w-[800px] bg-bkg-purple">
-            <div className="w-[50%] text-white ml-48 mt-24 text-5xl font-bold">
-              Your friend this week is...
-            </div>
-
-            <div className="text-white mt-[150px] ml-[750px] text-3xl font-bold">
-              {friendData.email}!
-            </div>
-
-            <div className="w-[30%] mt-[212px] ml-[px] text-white text-base">
-              Contact your friend of the week and choose together a quick
-              activity you’d like to do together. It can be just a 15 min online
-              coffee chat or any other entertainment to your taste. For
-              inspiration, we prepared a list of cool quick games you could try.
-              Go check it out!{" "}
-            </div>
+          <div className="absolute inset-0 z-0">
+            <img
+              className="relative z-10 h-1/1 w-full object-cover"
+              src={Background}
+              alt="Universe Background"
+            />
           </div>
-          <div className="text-white mt-[30px] text-center text-[32px] font-bold"></div>
-          Suggested Activities
-          <div className="grid gap-8 justify-center mt-[30px]">
-            {activity.map((activity) => (
-              <button class="w-[250px] h-[65px] bg-button-gray text-white font-bold rounded-[27px] shadow-md">
-                {activity}
-              </button>
-            ))}
+
+          <div className="relative z-10">
+            <NavBar />
+            <div className="flex">
+              <div className="w-2/3 h-full">
+                <div className="text-white ml-72 mt-20 text-5xl font-bold">
+                  Your friend this week is...
+                </div>
+
+                <div className="items-center mt-28 p-4 flex gap-8">
+                  <img
+                    className="ml-96 h-3/5"
+                    src={Avatar}
+                    alt="AvatarProfile"
+                  />
+
+                  <div className="text-white text-3xl font-bold">
+                    {friendData.email}!
+                  </div>
+
+                </div>
+
+                <div className="items-center mt-2 flex">
+                  <img
+                    className="h-3/5"
+                    src={AstronautPointing}
+                    alt="AstronautPointing"
+                  />
+
+                  <div className="w-[35%] text-base text-white">
+                      Contact your friend of the week and choose together a quick activity
+                      you’d like to do together. It can be just a 15 min online coffee chat
+                      or any other entertainment to your taste. For inspiration, we prepared
+                      a list of cool quick games you could try. Go check it out!{" "}
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-1/3 h-[1080px] bg-bkg-purple flex-shrink-0">
+                <div className="text-white mt-[30px] text-center text-[32px] font-bold">
+                  Suggested Activities
+                </div>
+
+                <div className="grid gap-8 justify-center mt-12">
+                  {activity.map((activity) => (
+                    <button class="w-72 h-16 bg-button-gray text-white font-bold rounded-[27px] shadow-md">
+                      {activity}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       )}
