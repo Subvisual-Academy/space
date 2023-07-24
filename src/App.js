cc326dd193f41f74aa357091de47f7550fef778d
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-24 18:17:25 +0100

    Update the register page to new design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Background from "./assets/universe_background.jpeg";
-import Logo from "./assets/spacecenter1.svg";
+import Background from "./assets/universe_background.png";
+import Logo from "./assets/logo.svg";
 import { useNavigate } from "react-router-dom";
 import { post } from "./Utils";
 
@@ -11,19 +11,13 @@ function App() {
     const data = new FormData(event.target);
     const mail = data.get("email");
     const pass = data.get("password");
-    const confirmEmail = data.get("confirmEmail");
+    const name = data.get("name");
     const confirmPassword = data.get("confirmPassword");
 
-    if (
-      mail &&
-      confirmEmail &&
-      pass &&
-      confirmPassword &&
-      mail === confirmEmail &&
-      pass === confirmPassword
-    ) {
+    if (mail && name && pass && confirmPassword && pass === confirmPassword) {
       const id = await post(`users`, {
         email: mail,
+        name: name,
         password: pass,
       }).then((response) => response["id"]);
       const tokenRes = await post(`auth/login`, {
@@ -40,6 +34,92 @@ function App() {
 
   return (
     <div className="flex items-start flex-auto bg-cod-gray">
+      <div className="p-8 flex flex-col grow items-start lg:max-h-screen mt-20 ml-32">
+        <img
+          className="max-h-screen basis-2/5 object-cover"
+          src={Logo}
+          alt="A background representing the Universe"
+        />
+        <h1 className=" mt-6 text-white text-4xl">Register your account</h1>
+        <form
+          name="register"
+          className="flex flex-col w-9/12 max-w-screen-sm gap-9 mt-12"
+          onSubmit={handleSubmit}
+        >
+          <div>
+            <label
+              for="email"
+              className="block text-base font-medium leading-6 text-white"
+            >
+              Email adress
+            </label>
+            <div class="mt-2">
+              <input
+                type="email"
+                name="email"
+                className="block w-full pl-3 rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              for="name"
+              className="block text-base font-medium leading-6 text-white"
+            >
+              Full Name
+            </label>
+            <div class="mt-2">
+              <input
+                type="text"
+                name="name"
+                className="block w-full pl-3 rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              for="password"
+              className="block text-base font-medium leading-6 text-white"
+            >
+              Password
+            </label>
+            <div class="mt-2">
+              <input
+                name="password"
+                type="password"
+                className="block w-full pl-3 rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              for="password"
+              className="block text-base font-medium leading-6 text-white"
+            >
+              Confirm Password
+            </label>
+            <div class="mt-2">
+              <input
+                name="confirmPassword"
+                type="password"
+                className="block w-full pl-3 rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            class="mt-1 text-base transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300 rounded-md bg-cerulean p-4 font-semibold text-white shadow-sm hover:bg-cerulean focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cerulean"
+          >
+            Register
+          </button>
+          <div className="text-silver text-2xl text-center">
+            Already have an account?{" "}
+            <a className="underline text-cerulean" href="/login">
+              Login
+            </a>
+          </div>
+        </form>
+      </div>
       <div className="hidden lg:block relative">
         <img
           className="max-h-screen basis-2/5 object-cover"
@@ -51,61 +131,6 @@ function App() {
           src={Logo}
           alt="Space Center Logo"
         />
-      </div>
-
-      <div className="p-8 flex flex-col grow items-center lg:max-h-screen mt-20">
-        {/* <button
-          type="button"
-          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300 w-1/2 max-w-screen-sm"
-        >
-          <div className="border-dove-gray border-2 rounded-3xl pt-4 text-silver text-xl h-16">
-            Continue with Google
-          </div>
-        </button> */}
-        <form
-          name="register"
-          className="flex flex-col w-9/12 max-w-screen-sm  gap-8 mt-12"
-          onSubmit={handleSubmit}
-        >
-          <input
-            name="email"
-            type="text"
-            placeholder="E-mail"
-            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
-          />
-          <input
-            name="confirmEmail"
-            type="text"
-            placeholder="Confirm e-mail"
-            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
-          />
-          <input
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm Password"
-            className="placeholder-gray placeholder:text-xl caret-gray text-gray pl-4 h-16 rounded-3xl bg-mine-shaft text-xl"
-          />
-          <button
-            type="submit"
-            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300"
-          >
-            <div className="bg-med-purple rounded-3xl pt-3 text-alto text-3xl h-16">
-              Submit
-            </div>
-          </button>
-          <div className="text-silver text-2xl text-center">
-            Already have an account?{" "}
-            <a className="underline text-blue-500" href="/login">
-              Login
-            </a>
-          </div>
-        </form>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,22 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["OpenSans", "sans-serif"],
-        audioWide: ["AudioWide"]
+        audioWide: ["AudioWide"],
       },
       colors: {
         "cod-gray": "#161616",
         "med-purple": "#8556e0",
-        "alto": "#dcdcdc",
-        "silver": "#bcb9b9",
-        "gray": "#8C8C8C",
+        alto: "#dcdcdc",
+        silver: "#bcb9b9",
+        gray: "#8C8C8C",
         "mine-shaft": "#303030",
         "dove-gray": "#636363",
         "input-purple": "#B27BD0",
         "dark-cyan": "#007B88",
         "register-cyan": "#40ACB8",
         "cornflower-blue": "#007B88",
-        "blackcurrant": "#403347"
+        blackcurrant: "#403347",
+        cerulean: "#06B4C6",
       },
     },
   },
