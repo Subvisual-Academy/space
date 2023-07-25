397aae01a2276561754e32243d1fff028abd4d83
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-25 10:48:27 +0100

    code review and fixing sizing of cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="bg-cod-gray absolute h-full w-full">
+    <div className="bg-cod-gray h-full w-full">
       <NavBar />
       <h1 className="text-white ml-32 mt-16 text-5xl">
         Welcome to the Space Center
@@ -17,9 +17,9 @@ function Home() {
         tasks every week and make new connections. And who knows, maybe it's
         here where you'll find some very good friends.
       </div>
-      <div className="ml-32 mt-32 flex gap-5 h-80 w-2/3 flex-1">
+      <div className="ml-32 mt-32 flex gap-5 h-80 w-full flex-shrink max-sm:flex-col">
         <Link to="/question">
-          <button className=" max-w-xl rounded-3xl bg-question text-white h-full bg-cover p-4 items-center bg-center">
+          <button className=" max-w-[510px] max-sm:max-w-[255px] rounded-3xl bg-question text-white p-4 h-full bg-cover bg-center">
             <h1 className="text-base">QUESTION OF THE WEEK</h1>
             <div className="text-sm">
               Get to know your community by sharing a glimpse of your life and
@@ -28,7 +28,7 @@ function Home() {
           </button>
         </Link>
         <Link to="/friend">
-          <button className=" max-w-xl rounded-3xl bg-friend text-white p-4 h-full bg-cover bg-center">
+          <button className=" max-w-[510px] max-sm:max-w-[255px] rounded-3xl bg-friend text-white p-4 h-full bg-cover bg-center">
             <h1 className="text-base">FRIEND OF THE WEEK</h1>
             <div className="text-sm">
               Get in touch with a member of the Subvisual Universe assigned to
