93df8c53449f2696ce9cb9a4d7f829e57a055475
Tiago Passos Rodrigues <63853699+TiagoPR@users.noreply.github.com>
2023-07-25 10:30:50 +0100

    Update the question page with more information and functionality (#17)
    Why:
    
    * User can only reply to the question 1 time and needs to know how much time he has left to answer it and know when did the others users responded. Change from `content` to `body` because of change on the backend.
    
    This change addresses the need by:
    
    * Creating helping constants so we can say which month we are in, using the Date constructor and reload page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,29 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "galaxy-pattern": "url('assets/galaxy.png')",
+        astronaut: "url('assets/sadAstronaut.png')",
+      },
       fontFamily: {
-        sans: ["OpenSans", "sans-serif"],
-        audioWide: ["AudioWide"]
+        sans: ["IBMPlexSans", "sans-serif"],
+        audioWide: ["AudioWide"],
+        openSans: ["OpenSans"],
       },
       colors: {
         "cod-gray": "#161616",
         "med-purple": "#8556e0",
-        "alto": "#dcdcdc",
-        "silver": "#bcb9b9",
-        "gray": "#8C8C8C",
+        alto: "#dcdcdc",
+        silver: "#bcb9b9",
+        gray: "#8C8C8C",
         "mine-shaft": "#303030",
         "dove-gray": "#636363",
         "input-purple": "#B27BD0",
         "dark-cyan": "#007B88",
         "register-cyan": "#40ACB8",
         "cornflower-blue": "#007B88",
-        "blackcurrant": "#403347"
+        blackcurrant: "#403347",
+        cerulean: "#06B4C6",
       },
     },
   },
