8f3e2c8f0cc84a0e130e03ba5a6c732717c81151
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-03 17:34:48 +0100

    Changes from BE with single request

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Question from "./pages/question";
 import Friend from "./pages/friend";
-import Previous from "./pages/previous";
+import Previous from "./pages/previous_questions";
 
 const router = createHashRouter([
   {
--- src/pages/previous_questions.js
+++ src/pages/previous_questions.js
@@ -4,15 +4,22 @@ import { GET } from "../utils/fetch";
 import { useEffect, useState } from "react";
 
 async function getQuestions() {
-  const weekly_questions = await GET("weekly_questions");
-  const questions = [];
-  for (const value in weekly_questions) {
-    questions.push({
-      id: weekly_questions[value].question_id,
-      body: weekly_questions[value].question.body,
-      time: new Date(weekly_questions[value].week),
-    });
-  }
+  const weeklyQuestions = await GET("weekly_questions");
+  const questions = weeklyQuestions.map((value) => {
+    return {
+      id: value.id,
+      body: value.question.body,
+      week: new Date(value.week),
+      answers: value.question.answers.map((answer) => {
+        return {
+          email: answer.user.email,
+          body: answer.body,
+          created_at: new Date(answer.created_at),
+          user: answer.user.email,
+        };
+      }),
+    };
+  });
   return questions;
 }
 
@@ -20,9 +27,8 @@ const options = {
   month: "long",
 };
 
-function Previous() {
+function PreviousQuestions() {
   const [questions, setQuestions] = useState([]);
-  const [answers, setAnswers] = useState({});
   const [answersVisibility, setAnswersVisibility] = useState({});
 
   useEffect(() => {
@@ -36,35 +42,7 @@ function Previous() {
       ...prevVisibility,
       [questionId]: !prevVisibility[questionId],
     }));
-
-    getAll(questionId);
   };
-
-  async function getAll(questionId) {
-    const answers = await GET("questions/" + questionId + "/answers");
-    await Promise.all(
-      answers.map((answer) => {
-        return new Promise((res) => {
-          GET("users/" + answer["user_id"].toString()).then((user) =>
-            res({ user, body: answer.body, time: new Date(answer.created_at) })
-          );
-        });
-      })
-    ).then((values) => {
-      const data = values.map((value) => {
-        return {
-          email: value.user.email,
-          body: value.body,
-          time: value.time,
-          user: value.user.id,
-        };
-      });
-      setAnswers((prevAnswers) => ({
-        ...prevAnswers,
-        [questionId]: data,
-      }));
-    });
-  }
 
   function renderDate(date) {
     const startDate = new Date(date.getTime());
@@ -96,7 +74,7 @@ function Previous() {
       </div>
 
       {questions
-        .sort((a, b) => b.time.getTime() - a.time.getTime())
+        .sort((a, b) => b.week.getTime() - a.week.getTime())
         .map((questionItem) => (
           <div
             className=" mt-16 ml-96 gap-8 w-4/6 overflow-scroll no-scrollbar"
@@ -105,10 +83,10 @@ function Previous() {
             <div className="border-b-2 border-white px-2 pb-8">
               <div className="flex gap-6">
                 <h1 className="text-white w-28 pb-8">
-                  {renderDate(questionItem.time)}
+                  {renderDate(questionItem.week)}
                 </h1>
                 <h1 className="text-white w-9">
-                  {questionItem.time.getFullYear()}
+                  {questionItem.week.getFullYear()}
                 </h1>
                 <h1 className="text-white text-xl w-full">
                   {questionItem.body}
@@ -124,21 +102,21 @@ function Previous() {
               </div>
               <div className="items-center overflow-scroll no-scrollbar max-h-96">
                 {answersVisibility[questionItem.id] &&
-                  answers[questionItem.id]?.map((item) => (
+                  questionItem.answers.map((answer) => (
                     <div
                       className="bg-dark-cyan p-4 flex flex-col rounded-3xl h-auto w-full text-white mb-3"
-                      key={item.body}
+                      key={answer.body}
                     >
-                      <div className="text-xl ml-14">{item.email}</div>
+                      <div className="text-xl ml-14">{answer.email}</div>
                       <div className="text-xs ml-14">
-                        {item.time
+                        {answer.created_at
                           .toLocaleString(undefined, options)
                           .toUpperCase() +
                           " " +
-                          item.time.getUTCDate()}
+                          answer.created_at.getUTCDate()}
                       </div>
 
-                      <div className="text-base pt-4">{item.body}</div>
+                      <div className="text-base pt-4">{answer.body}</div>
                     </div>
                   ))}
               </div>
@@ -149,4 +127,4 @@ function Previous() {
   );
 }
 
-export default Previous;
+export default PreviousQuestions;
