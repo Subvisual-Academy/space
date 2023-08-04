df7f96c199beaf9daffbc1907d600703f6a5bbd7
MFJess <jessica2001mf@gmail.com>
2023-08-04 18:34:24 +0100

    Change image in page of user without pair. Fix margins and sizes.

--- src/pages/friend.js
+++ src/pages/friend.js
@@ -17,7 +17,7 @@ import NavBar from "../components/navBar";
 import { GET } from "../utils/fetch";
 import { useState } from "react";
 
-const current_user_id = localStorage.getItem("current");
+const current_user_id = 10//localStorage.getItem("current");
 
 const getFriend = async () => {
   var response = await GET("weekly_friends/" + current_user_id);
@@ -73,56 +73,75 @@ function Friend() {
           ) : (
             <div>
               <div className="w-2/3 h-full">
-                <div className="text-white ml-72 mt-20 text-5xl font-bold">
+                <div className="text-white ml-[7.5rem] mt-[2.19rem] text-5xl font-bold">
                   Your friend this week is...
                 </div>
 
-                <div className="items-center mt-28 p-4 flex gap-8">
+                <div className="flex items-center mt-[4.31rem] ml-[24.25rem]">
                   <img
-                    className="ml-96 h-3/5"
+                    className="w-[6rem] h-[6rem] h-3/5"
                     src={Avatar}
                     alt="AvatarProfile"
                   />
 
-                  <div className="text-white text-3xl font-bold">
-                    {friendData.email}!
-                  </div>
+                  <div className="ml-[1rem] text-[1.25rem] text-white"> Name </div>
                 </div>
 
-                <div className="items-center mt-2 flex">
+                <div className="flex">
                   <img
-                    className="h-3/5"
+                    className="ml-12 w-[25.5rem] h-[34.5rem]"
                     src={AstronautPointing}
                     alt="AstronautPointing"
                   />
 
-                  <div className="w-[45%] text-base text-white">
-                    Contact your friend of the week and choose together a quick
-                    activity you'd like to do together. It can be just a 15 min
-                    online coffee chat or any other entertainment to your taste.
-                    For inspiration, we prepared a list of cool quick games you
-                    could try. Go check it out!{" "}
+                  <div className="items-center ml-2 flex">
+                    
+                    <div className="flex-col items-center">
+                      
+                      <div className="flex text-[1rem]">
+                        <div className="text-white"> Email: </div>
+                        <div className="ml-2 text-highlighted-button-blue"> example </div>
+                      </div>
+
+                      <div className="flex text-[1rem] text-white">
+                        <div className="text-white"> Discord: </div>
+                        <div className="ml-2 text-highlighted-button-blue"> example </div>
+                      </div>
+
+                      <div className="mt-[1rem] text-[1rem] text-white">
+                        Contact your friend of the week and choose together a quick
+                        activity you’d like to do together. It can be just a 15 min
+                        online coffee chat or any other entertainment to your taste.
+                        For inspiration, we prepared a list of cool quick games you
+                        could try. Go check it out!{" "}
+                      </div>
+                    </div>
                   </div>
+
                 </div>
+                
               </div>
             </div>
           )}
 
           <div className="w-1/3 h-[1080px] bg-bkg-purple flex-shrink-0">
-            <div className="text-white mt-20 text-center text-[32px] font-bold">
-              Suggested Activities
+            <div className="text-white mt-[2.19rem] text-center text-[1.25rem] font-bold">
+              Suggested Activities to do with Name
             </div>
 
-            <div className="grid gap-16 justify-center mt-12">
+            <div className="grid gap-[3.5rem] justify-center mt-[2.5rem]">
               <Link to="https://skribbl.io">
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${Skriblio})` }}
-                  onMouseOver={(e) =>
-                    (e.target.innerText =
-                      "Draw and guess what your friends are drawing")
-                  }
-                  onMouseLeave={(e) => (e.target.innerText = "Skribl.io")}
+                  onMouseOver={(e) => {
+                    e.target.innerText = "Draw and guess what your friends are drawing";
+                    e.target.style.fontSize = "1rem"; 
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.innerText = "Skribl.io";
+                    e.target.style.fontSize = "1.25rem"
+                  }}
                 >
                   Skribl.io
                 </button>
@@ -132,11 +151,14 @@ function Friend() {
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${CardGames})` }}
-                  onMouseOver={(e) =>
-                    (e.target.innerText =
-                      "Play from a selection of card games!")
-                  }
-                  onMouseLeave={(e) => (e.target.innerText = "Cards Games")}
+                  onMouseOver={(e) => {
+                    e.target.innerText = "Play from a selection of card games!";
+                    e.target.style.fontSize = "1rem";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.innerText = "Cards Games";
+                    e.target.style.fontSize = "1.25rem";
+                  }}
                 >
                   Cards Games
                 </button>
@@ -146,11 +168,14 @@ function Friend() {
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${Battleship})` }}
-                  onMouseOver={(e) =>
-                    (e.target.innerText =
-                      "Play the online version of the classic battleship game")
-                  }
-                  onMouseLeave={(e) => (e.target.innerText = "Battleship")}
+                  onMouseOver={(e) => {
+                    e.target.innerText = "Play the online version of the classic battleship game";
+                    e.target.style.fontSize = "1rem";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.innerText = "Battleship";
+                    e.target.style.fontSize = "1.25rem";
+                  }}
                 >
                   Battleship
                 </button>
@@ -160,13 +185,14 @@ function Friend() {
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${OnlineMiniGames})` }}
-                  onMouseOver={(e) =>
-                    (e.target.innerText =
-                      "Play a fun mini-game with your friend!")
-                  }
-                  onMouseLeave={(e) =>
-                    (e.target.innerText = "Online mini-games")
-                  }
+                  onMouseOver={(e) => {
+                    e.target.innerText = "Play a fun mini-game with your friend!";
+                    e.target.style.fontSize = "1rem";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.innerText = "Online mini-games";
+                    e.target.style.fontSize = "1.25rem";
+                  }}
                 >
                   Online mini-games
                 </button>
@@ -176,11 +202,14 @@ function Friend() {
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${Chess})` }}
-                  onMouseOver={(e) =>
-                    (e.target.innerText =
-                      "Play the classic chess game with your friend")
-                  }
-                  onMouseLeave={(e) => (e.target.innerText = "Chess")}
+                  onMouseOver={(e) => {
+                    e.target.innerText = "Play the classic chess game with your friend";
+                    e.target.style.fontSize = "1rem";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.innerText = "Chess";
+                    e.target.style.fontSize = "1.25rem";
+                  }}
                 >
                   Chess
                 </button>
@@ -190,10 +219,14 @@ function Friend() {
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${TriviaGames})` }}
-                  onMouseOver={(e) =>
-                    (e.target.innerText = "Answer questions about any topic!")
-                  }
-                  onMouseLeave={(e) => (e.target.innerText = "Trivia Games")}
+                  onMouseOver={(e) => {
+                    e.target.innerText = "Answer questions about any topic!";
+                    e.target.style.fontSize = "1.25rem"
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.innerText = "Trivia Games";
+                    e.target.style.fontSize = "1rem";
+                  }}
                 >
                   Trivia Games
                 </button>
