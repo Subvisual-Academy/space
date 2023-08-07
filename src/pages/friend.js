712a3b4cd7e81535a5290c318478f4ca984ef4b6
MFJess <jessica2001mf@gmail.com>
2023-08-07 11:38:44 +0100

    Adjust details.

--- src/pages/friend.js
+++ src/pages/friend.js
@@ -17,7 +17,7 @@ import NavBar from "../components/navBar";
 import { GET } from "../utils/fetch";
 import { useState } from "react";
 
-const current_user_id = 10//localStorage.getItem("current");
+const current_user_id = 8; //localStorage.getItem("current");
 
 const getFriend = async () => {
   var response = await GET("weekly_friends/" + current_user_id);
@@ -40,31 +40,35 @@ function Friend() {
 
   return (
     <div>
-      <div 
+      <div
         className="bg-cover bg-center h-screen"
-        style={{ backgroundImage: `url(${Background})` }}>
-
+        style={{ backgroundImage: `url(${Background})` }}
+      >
         <NavBar />
 
         <div className="flex">
           {notPaired ? (
             <div className="w-2/3 h-full">
-              <div className="text-white ml-72 mt-20 text-5xl font-bold">
-                Vacation week!
-              </div>
+              <div className="ml-[7.5rem]">
+                <div className="text-white mt-[1.94rem] text-5xl font-bold">
+                  Vacation week!
+                </div>
 
-              <div className="w-[50%] ml-72 mt-16 text-base text-white">
-                Yey, look at you! So lucky, this week you've got a little
-                vacation from this task. Now, when you have more time, make sure
-                you answer the question of this week and relax. See you next
-                week, we've got someone very cool in store for you.{" "}
-              </div>
+                <div className="w-[37.375rem] mt-[1.25rem] text-base text-white">
+                  Well, there ain’t one week like the other, right? 
+                  <br />
+                  <br /> This time you haven’t got anyone, no worries. Maybe,
+                  it’s just a sign from the Universe to have some me-time, get a
+                  nice drink and devote 15 min to yourself.Don’t forget to
+                  answer the question of the week and get back next week!n{" "}
+                </div>
 
-              <img
-                className="ml-96 mt-12 h-3/5 relative z-10"
-                src={AstronautChilling}
-                alt="AstronautChilling"
-              />
+                <img
+                  className="mt-12 h-3/5 relative z-10"
+                  src={AstronautChilling}
+                  alt="AstronautChilling"
+                />
+              </div>
             </div>
           ) : (
             <div>
@@ -80,7 +84,10 @@ function Friend() {
                     alt="AvatarProfile"
                   />
 
-                  <div className="ml-[1rem] text-[1.25rem] text-white"> {friendData.name} </div>
+                  <div className="ml-[1rem] text-[1.25rem] text-white">
+                    {" "}
+                    {friendData.name}{" "}
+                  </div>
                 </div>
 
                 <div className="flex">
@@ -91,52 +98,61 @@ function Friend() {
                   />
 
                   <div className="items-center ml-2 flex">
-                    
                     <div className="flex-col items-center">
-
                       <div className="flex text-[1rem]">
                         <div className="text-white"> Email: </div>
-                        <div className="ml-2 text-highlighted-button-blue"> {friendData.email} </div>
+                        <div className="ml-2 text-highlighted-button-blue">
+                          {" "}
+                          {friendData.email}{" "}
+                        </div>
                       </div>
 
                       <div className="flex text-[1rem] text-white">
                         <div className="text-white"> Discord: </div>
-                        <div className="ml-2 text-highlighted-button-blue"> {friendData.discord} </div>
+                        <div className="ml-2 text-highlighted-button-blue">
+                          {" "}
+                          {friendData.discord}{" "}
+                        </div>
                       </div>
 
                       <div className="mt-[1rem] text-[1rem] text-white">
-                        Contact your friend of the week and choose together a quick
-                        activity you’d like to do together. It can be just a 15 min
-                        online coffee chat or any other entertainment to your taste.
-                        For inspiration, we prepared a list of cool quick games you
-                        could try. Go check it out!{" "}
+                        Contact your friend of the week and choose together a
+                        quick activity you’d like to do together. It can be just
+                        a 15 min online coffee chat or any other entertainment
+                        to your taste. For inspiration, we prepared a list of
+                        cool quick games you could try. Go check it out!{" "}
                       </div>
                     </div>
                   </div>
-
                 </div>
-                
               </div>
             </div>
           )}
 
           <div className="w-1/3 h-[1080px] bg-bkg-purple flex-shrink-0">
-            <div className="text-white mt-[2.19rem] text-center text-[1.25rem] font-bold">
-              Suggested Activities to do with {friendData.name}
-            </div>
-
+            {notPaired ? (
+              <div className="text-white mt-[2.19rem] text-center text-[1.25rem] font-bold"> 
+                Suggested Activities for next week
+              </div>
+            ) : (
+              <div className="text-white mt-[2.19rem] text-center text-[1.25rem] font-bold">
+                Suggested Activities to do with {friendData.name}
+              </div>
+            )}
+              
             <div className="grid gap-[3.5rem] justify-center mt-[2.5rem]">
               <Link to="https://skribbl.io">
                 <button
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${Skriblio})` }}
                   onMouseOver={(e) => {
-                    e.target.innerText = "Draw and guess what your friends are drawing";
-                    e.target.style.fontSize = "1rem"; 
+                    e.target.innerText =
+                      "Draw and guess what your friends are drawing";
+                    e.target.style.fontSize = "1rem";
                   }}
                   onMouseLeave={(e) => {
                     e.target.innerText = "Skribl.io";
-                    e.target.style.fontSize = "1.25rem"
+                    e.target.style.fontSize = "1.25rem";
                   }}
                 >
                   Skribl.io
@@ -165,7 +181,8 @@ function Friend() {
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${Battleship})` }}
                   onMouseOver={(e) => {
-                    e.target.innerText = "Play the online version of the classic battleship game";
+                    e.target.innerText =
+                      "Play the online version of the classic battleship game";
                     e.target.style.fontSize = "1rem";
                   }}
                   onMouseLeave={(e) => {
@@ -182,7 +199,8 @@ function Friend() {
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${OnlineMiniGames})` }}
                   onMouseOver={(e) => {
-                    e.target.innerText = "Play a fun mini-game with your friend!";
+                    e.target.innerText =
+                      "Play a fun mini-game with your friend!";
                     e.target.style.fontSize = "1rem";
                   }}
                   onMouseLeave={(e) => {
@@ -199,7 +217,8 @@ function Friend() {
                   className="relative flex flex-col items-center justify-center w-72 h-20 rounded-[27px] text-white text-lg font-bold bg-cover bg-center focus:border-4 focus:border-white"
                   style={{ backgroundImage: `url(${Chess})` }}
                   onMouseOver={(e) => {
-                    e.target.innerText = "Play the classic chess game with your friend";
+                    e.target.innerText =
+                      "Play the classic chess game with your friend";
                     e.target.style.fontSize = "1rem";
                   }}
                   onMouseLeave={(e) => {
@@ -217,11 +236,11 @@ function Friend() {
                   style={{ backgroundImage: `url(${TriviaGames})` }}
                   onMouseOver={(e) => {
                     e.target.innerText = "Answer questions about any topic!";
-                    e.target.style.fontSize = "1.25rem"
+                    e.target.style.fontSize = "1rem";
                   }}
                   onMouseLeave={(e) => {
                     e.target.innerText = "Trivia Games";
-                    e.target.style.fontSize = "1rem";
+                    e.target.style.fontSize = "1.25rem";
                   }}
                 >
                   Trivia Games
