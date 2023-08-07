315b188045f0c20697828cbedd8dc12956af1be0
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-07 11:44:47 +0100

    requested changes

--- src/pages/previous_questions.js
+++ src/pages/previous_questions.js
@@ -4,23 +4,7 @@ import { GET } from "../utils/fetch";
 import { useEffect, useState } from "react";
 
 async function getQuestions() {
-  const weeklyQuestions = await GET("weekly_questions");
-  const questions = weeklyQuestions.map((value) => {
-    return {
-      id: value.id,
-      body: value.question.body,
-      week: new Date(value.week),
-      answers: value.question.answers.map((answer) => {
-        return {
-          email: answer.user.email,
-          body: answer.body,
-          created_at: new Date(answer.created_at),
-          user: answer.user.email,
-        };
-      }),
-    };
-  });
-  return questions;
+  return await GET("weekly_questions");
 }
 
 const options = {
@@ -44,24 +28,6 @@ function PreviousQuestions() {
     }));
   };
 
-  function renderDate(date) {
-    const startDate = new Date(date.getTime());
-    const endDate = new Date(date.getTime());
-
-    const daysToSubtract = startDate.getDay() + 7;
-
-    startDate.setDate(startDate.getDate() - daysToSubtract + 1);
-    endDate.setDate(endDate.getDate() - startDate.getDay());
-
-    const startDay = startDate.getDate();
-    const startMonth = startDate.getMonth() + 1;
-
-    const endDay = endDate.getDate();
-    const endMonth = endDate.getMonth() + 1;
-
-    return `${startDay}/${startMonth}-${endDay}/${endMonth}`;
-  }
-
   return (
     <div className="bg-previous min-h-screen bg-no-repeat bg-cover">
       <NavBar />
@@ -74,7 +40,7 @@ function PreviousQuestions() {
       </div>
 
       {questions
-        .sort((a, b) => b.week.getTime() - a.week.getTime())
+        .sort((a, b) => new Date(b.week) - new Date(a.week))
         .map((questionItem) => (
           <div
             className=" mt-16 ml-96 gap-8 w-4/6 overflow-scroll no-scrollbar"
@@ -82,14 +48,12 @@ function PreviousQuestions() {
           >
             <div className="border-b-2 border-white px-2 pb-8">
               <div className="flex gap-6">
-                <h1 className="text-white w-28 pb-8">
-                  {renderDate(questionItem.week)}
-                </h1>
+                <h1 className="text-white w-32 pb-8">{questionItem.during}</h1>
                 <h1 className="text-white w-9">
-                  {questionItem.week.getFullYear()}
+                  {new Date(questionItem.week).getFullYear()}
                 </h1>
                 <h1 className="text-white text-xl w-full">
-                  {questionItem.body}
+                  {questionItem.question.body}
                 </h1>
                 <img
                   src={Chevron}
@@ -102,18 +66,18 @@ function PreviousQuestions() {
               </div>
               <div className="items-center overflow-scroll no-scrollbar max-h-96">
                 {answersVisibility[questionItem.id] &&
-                  questionItem.answers.map((answer) => (
+                  questionItem.question.answers.map((answer) => (
                     <div
                       className="bg-dark-cyan p-4 flex flex-col rounded-3xl h-auto w-full text-white mb-3"
                       key={answer.body}
                     >
-                      <div className="text-xl ml-14">{answer.email}</div>
+                      <div className="text-xl ml-14">{answer.user.email}</div>
                       <div className="text-xs ml-14">
-                        {answer.created_at
+                        {new Date(answer.created_at)
                           .toLocaleString(undefined, options)
                           .toUpperCase() +
                           " " +
-                          answer.created_at.getUTCDate()}
+                          new Date(answer.created_at).getUTCDate()}
                       </div>
 
                       <div className="text-base pt-4">{answer.body}</div>
