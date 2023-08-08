e651b912eff62ae8d8f5148c586b59404f166125
jess <61982705+MFJess@users.noreply.github.com>
2023-08-08 17:54:05 +0100

    Fix unauthorized login. (#35)
    * Add condition to check successful login.
    
    * Add credentials verification in login.
    
    * Add pading to inputs.
    
    * Correct input text size.
    
    * Code check.
    
    * Fix spacings and measurement units.
    
    * Change input font size.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import Background from "../assets/space-background.png";
 import Logo from "../assets/login-logo.png";
 import { useNavigate } from "react-router-dom";
-import { post } from "../Utils";
+import { POST } from "../utils/fetch";
 
 function Login() {
   const navigate = useNavigate();
@@ -13,23 +13,27 @@ function Login() {
     const pass = data.get("password");
 
     if (mail && pass) {
-      await post(`auth/login`, {
+      POST(`auth/login`, {
         email: mail,
         password: pass,
-      }).then((response) => {
-        localStorage.setItem("token", response["token"]);
-        localStorage.setItem("current", response["user"]);
-      });
-      navigate("/home");
-      navigate(0);
+      })
+        .then((response) => {
+          localStorage.setItem("token", response["token"]);
+          localStorage.setItem("current", response["user"]);
+          navigate("/home");
+        })
+        .catch((error) => {
+          console.error("Error occurred during login:", error.message);
+          alert("Invalid credentials. Please, try again.");
+        });
     } else {
-      alert("E-mail or Password doesn't match!");
+      alert("Please enter both email and password.");
     }
   };
 
   return (
     <div className="flex bg-cod-gray">
-      <div className="flex-col ml-32 w-1/2 mt-80">
+      <div className="flex-col ml-32 w-1/2 h-full mt-44">
         <img className="w-64 h-14" src={Logo} alt="Logo" />
 
         <div className="text-4xl font-bold text-white mt-6">
@@ -46,7 +50,7 @@ function Login() {
             id="email"
             name="email"
             type="text"
-            className="bg-white rounded-md w-[422px] h-[38px] hover:border-4 hover:border-blue-login active:border-4 active:border-blue-login placeholder:text-gray"
+            className="bg-white text-base rounded-md w-3/4 h-10 hover:border-4 hover:border-blue-login active:border-4 active:border-blue-login placeholder:text-gray p-2"
             placeholder="you@example.com"
           />
 
@@ -55,37 +59,47 @@ function Login() {
             id="password"
             name="password"
             type="password"
-            className="bg-white rounded-md w-[422px] h-[38px] hover:border-4 hover:border-blue-login active:border-4 active:border-blue-login placeholder:text-gray"
+            className="bg-white text-base rounded-md w-3/4 h-10 hover:border-4 hover:border-blue-login active:border-4 active:border-blue-login placeholder:text-gray p-2"
             placeholder="your password"
           />
 
           <button
             type="submit"
-            className="bg-blue-login rounded-md w-[422px] h-[38px] text-white text-base font-bold mt-10 hover:bg-dark-cyan focus:border-4 focus:border-white"
+            className="bg-blue-login rounded-md w-3/4 h-10 text-white text-base font-bold mt-10 hover:bg-dark-cyan focus:border-4 focus:border-white"
           >
             Login
           </button>
 
-          <div className="text-light-gray text-base mt-8 ml-24">
+          <div className="text-light-gray text-base mt-8 ml-40">
             Don't have an account?{" "}
-            <a className="underline text-blue-login" href="/space">
+            <a className="text-blue-login" href="/space">
               Register
             </a>
           </div>
+
+          <a className="text-blue-login text-base mt-2 ml-48" href="/password">
+            Forgot your password?
+          </a>
         </form>
       </div>
 
-      <div className="flex-shrink-0 w-1/2 hidden md:block relative">
-        <img
-          className="h-full w-full"
-          src={Background}
-          alt="Universe Background"
-        />
+      <div
+        className="w-1/2"
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <img className="mt-96 ml-40" src={Logo} alt="Space Center Logo" />
+        {/*
         <img
           className="absolute top-[calc(50%-theme(space.96)/4)] left-[calc(50%-theme(space.72)/1.25)]"
           src={Logo}
           alt="Space Center Logo"
         />
+  */}
       </div>
     </div>
   );
