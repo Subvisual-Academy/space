85cb029f2c8e485fb894da51d128294b08156c60
MFJess <jessica2001mf@gmail.com>
2023-08-17 12:26:24 +0100

    Run format.

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -81,7 +81,11 @@ function Profile() {
       <div className="flex">
         <div className="flex-none w-2/5">
           <div className="flex-col ml-32 mt-16">
-            <img className="h-60 w-60 rounded-lg" src={localStorage.getItem("image")} alt="User profile avatar" />
+            <img
+              className="h-60 w-60 rounded-lg"
+              src={localStorage.getItem("image")}
+              alt="User profile avatar"
+            />
 
             <div className="mt-8 mb-8 text-white text-3xl">{userData.name}</div>
 
